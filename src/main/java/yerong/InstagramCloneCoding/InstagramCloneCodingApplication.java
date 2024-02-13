f06c81765b844773686953c42b52172166ac3386
nyeroni <nir2y@naver.com>
2024-02-14 01:38:36 +0900

    Initial Commit

---
+++ src/main/java/yerong/InstagramCloneCoding/InstagramCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package yerong.InstagramCloneCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneCodingApplication.class, args);
+	}
+
+}
