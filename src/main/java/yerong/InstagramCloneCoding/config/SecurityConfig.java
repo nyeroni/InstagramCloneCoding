6bed174399215dd36c88ca8029040066351ebe23
nyeroni <nir2y@naver.com>
2024-02-16 14:31:43 +0900

    feat: security 설정

---
+++ src/main/java/yerong/InstagramCloneCoding/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package yerong.InstagramCloneCoding.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(authorizationRequest ->
+                        authorizationRequest
+                                .requestMatchers(
+                                        AntPathRequestMatcher.antMatcher("/"),
+                                        AntPathRequestMatcher.antMatcher("/user/**"),
+                                        AntPathRequestMatcher.antMatcher("/image/**"),
+                                        AntPathRequestMatcher.antMatcher("/subscribe/**"),
+                                        AntPathRequestMatcher.antMatcher("/comment/**")
+                                ).authenticated()
+                                .anyRequest().permitAll()
+                )
+                .formLogin(formConfig ->
+                        formConfig.loginPage("/auth/signin")
+                                .defaultSuccessUrl("/"))
+                .build();
+    }
+}
