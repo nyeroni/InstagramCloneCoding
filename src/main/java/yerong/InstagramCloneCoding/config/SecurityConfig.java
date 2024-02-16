03d368119c33cfc51eefd1891094c8c178b0d77f
nyeroni <nir2y@naver.com>
2024-02-16 17:21:29 +0900

    feat: BCryptPasswordEncoder 빈에 등록

--- src/main/java/yerong/InstagramCloneCoding/config/SecurityConfig.java
+++ src/main/java/yerong/InstagramCloneCoding/config/SecurityConfig.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig{
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
         return http
