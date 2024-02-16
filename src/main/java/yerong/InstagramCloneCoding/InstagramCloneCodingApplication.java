3dacb6f957f02ad37396267ddbd32c91ae4c2536
nyeroni <nir2y@naver.com>
2024-02-16 17:27:01 +0900

    feat: BaseTimeEntity를 사용하기 JPA Auditing 활성화

--- src/main/java/yerong/InstagramCloneCoding/InstagramCloneCodingApplication.java
+++ src/main/java/yerong/InstagramCloneCoding/InstagramCloneCodingApplication.java
@@ -2,7 +2,9 @@ package yerong.InstagramCloneCoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class InstagramCloneCodingApplication {
 
