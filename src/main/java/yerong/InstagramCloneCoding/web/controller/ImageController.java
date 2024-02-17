135efd085fad46dbf02be9a3be2ff3f27b654c2d
nyeroni <nir2y@naver.com>
2024-02-17 17:46:40 +0900

    Feat: 메인 화면 및 인기 페이지 url 매핑

---
+++ src/main/java/yerong/InstagramCloneCoding/web/controller/ImageController.java
@@ -0,0 +1,18 @@
+package yerong.InstagramCloneCoding.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ImageController {
+
+    @GetMapping({"/", "/image/story"})
+    public String story(){
+        return "views/image/story";
+    }
+    @GetMapping("/image/popular")
+    public String popular(){
+        return "views/image/popular";
+    }
+
+}
