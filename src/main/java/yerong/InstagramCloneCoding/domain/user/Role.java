84b11cac6453ab4576da8e218df8f657f6e0ea35
nyeroni <nir2y@naver.com>
2024-02-17 17:10:09 +0900

    Refactor: 오타수정

--- src/main/java/yerong/InstagramCloneCoding/domain/user/Role.java
+++ src/main/java/yerong/InstagramCloneCoding/domain/user/Role.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @AllArgsConstructor
 public enum Role {
-    USER("ROLE_UESR", "회원"),
+    USER("ROLE_USER", "회원"),
     ADMIN("ROLE_ADMIN", "관리자");
     private String key;
     private String title;
